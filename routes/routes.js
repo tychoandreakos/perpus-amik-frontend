bbe23c45016f0e9ffdb8fe8f4da9a2bca5f03a9e
Elang Indra <tlurie@student.sierracollege.edu>
2020-02-07 10:24:41 +0700

    fixed HeroComponent

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,48 @@
 import LandingComponent from "../src/components/Page/Landing";
 import AccountComponent from "../src/components/Page/Account";
 
+const title = "Diglib STMIK AMIK BANDUNG";
+
+const putTitle = pageTitle => {
+  return `${pageTitle} - ${title}`;
+};
+
 const router = [
   {
     path: "/",
     name: "landing",
-    component: LandingComponent
+    component: LandingComponent,
+    meta: {
+      title,
+      metaTags: [
+        {
+          name: "description",
+          content: "The about page of our example app."
+        },
+        {
+          property: "og:description",
+          content: "The about page of our example app."
+        }
+      ]
+    }
   },
   {
     path: "/account",
     name: "Account",
-    component: AccountComponent
+    component: AccountComponent,
+    meta: {
+      title: putTitle("Account"),
+      metaTags: [
+        {
+          name: "description",
+          content: "The about page of our example app."
+        },
+        {
+          property: "og:description",
+          content: "The about page of our example app."
+        }
+      ]
+    }
   }
 ];
 
