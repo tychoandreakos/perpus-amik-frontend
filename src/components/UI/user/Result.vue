a58aa9577622c7a2a3c8ca5a06e7c8c3938c330c
Elang Indra <tlurie@student.sierracollege.edu>
2020-02-17 18:30:21 +0700

    yes, finished library result panel

--- src/components/UI/user/Result.vue
+++ src/components/UI/user/Result.vue
@@ -29,9 +29,13 @@
         <span class="info">ISBN: {{ bookResult.isbn }}</span>
         <span class="info"
           >Topic:
-          <router-link to="#" v-for="(topic, i) in bookResult.topic" :key="i">{{
-            topic.toUpperCase()
-          }}</router-link></span
+          <router-link
+            class="topic"
+            to="#"
+            v-for="(topic, i) in bookResult.topic"
+            :key="i"
+            >{{ topic.toUpperCase() }}</router-link
+          ></span
         >
       </div>
       <div class="button-info">
@@ -40,6 +44,11 @@
         <router-link to="#">Adding to Playlist</router-link>
       </div>
     </div>
+
+    <section class="description-book">
+      <h3>Book Description</h3>
+      <div class="desc" v-html="description"></div>
+    </section>
   </section>
 </template>
 <script>
@@ -54,6 +63,8 @@ export default {
   },
   data() {
     return {
+      description:
+        '<p>Why is PHP the most widely used programming language on the web? This updated edition teaches everything you need to know to create effective web applications using the latest features in PHP 7.3. You’ll start with the big picture and then dive into language syntax, programming techniques, and other details, using examples that illustrate both correct usage and common idioms.</p><br /><p>If you have a working knowledge of HTML, authors Kevin Tatroe and Peter MacIntyre provide many style tips and practical programming advice in a clear and concise manner to help you become a top-notch PHP programmer.</p><br /><ul><li>Understand what’s possible when you use PHP programs</li><li>Learn language fundamentals, including data types, variables, operators, and flow control statements</li><li>Explore functions, strings, arrays, and objects</li><li>Apply common web application techniques, such as form processing, data validation, session tracking, and cookies</li><li>Interact with relational databases like MySQL or NoSQL databases such as MongoDB</li><li>Generate dynamic images, create PDF files, and parse XML files</li><li>Learn secure scripts, error handling, performance tuning, and other advanced topics</li><li>Get a quick reference to PHP core functions and standard extensions</li></ul>',
       title: 'Start Reading Now',
       bookResult: {
         title: 'Programming PHP, 4th Edition',
@@ -73,6 +84,8 @@ export default {
   margin: 0 2rem;
   padding: 2rem;
   background: #fff;
+  box-shadow: 0 5px 10px #e3e3e3;
+  margin-bottom: 2rem;
 }
 
 #result-book .info-book {
@@ -117,7 +130,8 @@ export default {
   display: block;
 }
 
-#result-book .info-book .book-description .author {
+#result-book .info-book .book-description .author,
+#result-book .info-book .book-description span.info .topic {
   color: #613ff4;
 }
 
@@ -139,6 +153,10 @@ export default {
   color: #949090;
 }
 
+#result-book .info-book .book-description span.info .topic:hover {
+  color: #222;
+}
+
 #result-book .info-book .button-info {
   align-self: center;
   align-items: center;
@@ -153,12 +171,33 @@ export default {
 
 #result-book .info-book .button-info a {
   font-family: 'Nunito', sans-serif;
-  font-size: 0.9rem;
+  font-size: 0.8rem;
   text-decoration: none;
   margin: 0.2rem 0;
+  color: #949090;
 }
 
 #result-book .info-book .button-info a:hover {
   color: #222;
+}
+
+#result-book .description-book {
+  margin-top: 2.5rem;
+}
+
+#result-book .description-book h3 {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  font-size: 1.6rem;
+}
+
+#result-book .description-book .desc {
+  margin-top: 1rem;
+  font-family: 'Nunito', sans-serif;
+  font-size: 0.9rem;
+}
+
+#result-book .description-book .desc >>> ul {
+  margin-left: 1.5rem;
 }
 </style>
