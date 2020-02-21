08599380b6bfa18fe7ad362cc3a47ca1f624f667
Elang Indra <tlurie@student.sierracollege.edu>
2020-02-22 00:57:47 +0700

    fix length of breadcrumbs

--- src/components/Page/admin/CirculationTransaction.vue
+++ src/components/Page/admin/CirculationTransaction.vue
@@ -1,10 +1,40 @@
 <template>
-  <section id="circulation-transaction"></section>
+  <section id="circulation-transaction">
+    <CardComponent :cardProperties="styleCard">
+      <section class="user">
+        <div class="img"></div>
+        <div class="profil">
+          <StripedTableComponent>
+            <thead v-if="userList.includeHead">
+              <th v-for="(head, i) in userList.head" :key="i">{{ head }}</th>
+            </thead>
+            <tbody>
+              <tr v-for="(body, i) in userList.body" :key="i">
+                <td>{{ i }}</td>
+                <td>{{ body }}</td>
+              </tr>
+            </tbody>
+          </StripedTableComponent>
+        </div>
+      </section>
+    </CardComponent>
+    <div class="transaction">
+      <CardComponent :cardProperties="styleCard">
+        x
+      </CardComponent>
+    </div>
+  </section>
 </template>
 <script>
+import CardComponent from '../../UI/admin/Card';
+import StripedTableComponent from '../../UI/admin/table/Striped';
+
 export default {
   name: 'CirculationTransaction',
-  components: {},
+  components: {
+    CardComponent,
+    StripedTableComponent
+  },
   mounted() {
     this.$store.commit('setCirculation', {
       title: this.title,
@@ -14,9 +44,55 @@ export default {
   data() {
     return {
       breadcrumbs: ['Search Member ID', '1702018 - Elang Indra'],
-      title: 'Circulation'
+      title: 'Circulation',
+      styleCard: {
+        background: '#fff'
+      },
+      userList: {
+        includeHead: false,
+        body: {
+          'Member Name': 'Elang Indra',
+          'Member Email': 'elangindra15@gmail.com',
+          'Register Date': '2020-02-19',
+          'Member ID': 1702018,
+          'Member Type': 'Mahasiswa',
+          'Expiry Date': '2021 - 02 - 18'
+        }
+      }
     };
   }
 };
 </script>
-<style scoped></style>
+<style scoped>
+#circulation-transaction {
+  width: 100%;
+}
+
+#circulation-transaction .transaction {
+  margin-top: 3rem;
+}
+
+#circulation-transaction .user .img {
+  width: 250px;
+  height: 250px;
+  flex-basis: 37%;
+  background: url('https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/products/01.png');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  margin-right: 3%;
+}
+
+#circulation-transaction .user .profil {
+  width: 100%;
+  flex-basis: 60%;
+}
+
+#circulation-transaction .user {
+  padding: 1rem;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  height: 100%;
+}
+</style>
---
+++ src/components/UI/admin/table/Striped.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="striped">
+    <table>
+      <slot></slot>
+    </table>
+  </section>
+</template>
+<script>
+export default {
+  name: 'Striped',
+  props: {
+    tableData: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+<style scoped>
+.striped {
+  width: 100%;
+  height: 100%;
+}
+
+.striped table {
+  border: 1px solid #f8f8f8;
+  font-family: 'Quicksand', sans-serif;
+  width: 100%;
+  border-collapse: separate;
+}
+
+.striped table th {
+  text-transform: capitalize;
+  font-size: 0.9rem;
+  padding: 0.7rem 0;
+}
+
+.striped table tr:nth-child(odd) {
+  background: #f8f8f8;
+}
+
+.striped table tr {
+  transition: transform 0.3s ease;
+}
+
+.striped table tr:hover {
+  transform: translateX(5px);
+}
+
+.striped table tr td {
+  padding: 0.8rem 0.5rem;
+  font-size: 0.94rem;
+}
+</style>
