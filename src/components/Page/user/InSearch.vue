5b1e60abbf1b9f32236407087925731f63266ae7
Elang Indra <tlurie@student.sierracollege.edu>
2020-04-22 04:47:36 +0700

    working with advancedSearch

--- src/components/Page/user/InSearch.vue
+++ src/components/Page/user/InSearch.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
   section#in-search
+    p.found #[strong 1 - 10 of 12541] search results for 
+      strong php
+    div.advanced
+      AdvancedSearch(v-for="(labels, i) in labelsSearch" :key="i" :label="labels")
     template(v-for="(book, i) in items")
       ListComponent(:item="book" :key="i")
     FooterComponent.footer
@@ -7,26 +11,50 @@
 <script>
 import ListComponent from "../../UI/user/ListBook";
 import FooterComponent from "../../UI/user/Footer";
+import AdvancedSearch from "../../UI/user/AdvancedSearch";
 
 export default {
   name: "InSearch",
   components: {
     ListComponent,
-    FooterComponent
+    FooterComponent,
+    AdvancedSearch,
+  },
+  data() {
+    return {
+      labelsSearch: ["formats", "topic", "publisher", "rating"],
+    };
   },
   computed: {
     items() {
       return this.$store.state.book;
-    }
-  }
+    },
+  },
 };
 </script>
-<style scoped>
+<style lang="scss">
 #in-search {
   margin-top: 2rem;
-}
 
-.footer {
+  .advanced {
+    justify-content: center;
+    margin: 1rem 1.2rem 0 0;
+    width: 100%;
+    display: flex;
+  }
+
+  p.found {
+    margin-left: 4rem;
+    display: inline;
+    font: {
+      size: 0.9rem;
+      family: "Poppins", sans-serif;
+      weight: 400;
+    }
+  }
+
+  .footer {
     margin-top: 2.5rem;
+  }
 }
 </style>
---
+++ src/components/UI/user/AdvancedSearch.vue
@@ -0,0 +1,114 @@
+<template lang="pug">
+    div#advanced-search(@click="dropdownHandler" :style="handler ? dropdownTrue : dropdownFalse ")
+        div.wrapper
+            span {{ label | capitalize }} 
+            Icon(icon="angle-down")
+        ul
+            li 
+                input#one(type="checkbox") 
+                label(for="one") All
+            li 
+                input#two(type="checkbox") 
+                label(for="two") Books (1209)
+        div.backdrop(v-if="handler" @click.stop="handler = !handler")
+                
+</template>
+
+<script>
+import Icon from "vue-themify-icons";
+export default {
+  name: "AdvancedSearch",
+  components: {
+    Icon,
+  },
+  methods: {
+    dropdownHandler() {
+      this.handler = !this.handler;
+    },
+  },
+  data() {
+    return {
+      handler: false,
+      dropdownTrue: {
+        height: "120px",
+      },
+      dropdownFalse: {
+        height: "40px",
+      },
+    };
+  },
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#advanced-search {
+  border: 1px solid #ccc;
+  padding: 0.5rem 1.5rem;
+  margin-left: 1.5rem;
+  cursor: pointer;
+  overflow: hidden;
+  transition: height 0.3s ease-in-out;
+
+  &:hover {
+    box-shadow: 0 10px 15px 0 rgba(40, 44, 53, 0.06),
+      0 2px 2px 0 rgba(40, 44, 53, 0.08);
+  }
+
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: rgba(255, 255, 255, 0.329);
+    z-index: 2;
+    cursor: default;
+  }
+
+  ul {
+    margin-top: 0.9rem;
+    list-style: none;
+    position: relative;
+    z-index: 5;
+    background: #fff;
+    li {
+      margin-bottom: 0.8rem;
+      display: flex;
+      align-items: center;
+      font: {
+        size: 0.9rem;
+        family: "Poppins", sans-serif;
+      }
+
+      label {
+        margin-left: 0.9rem;
+        display: inline-block;
+        width: 100%;
+        cursor: pointer;
+      }
+    }
+  }
+  .wrapper {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    display: inline-block;
+    transition: 0.2s box-shadow ease;
+    width: 100%;
+    position: relative;
+    z-index: 5;
+    background: #fff;
+
+    font: {
+      size: 0.9rem;
+      weight: 500;
+    }
+  }
+}
+</style>
