85fd604659a88e112aa24e99ea7ccbea179e990f
Elang Indra <tlurie@student.sierracollege.edu>
2020-04-21 14:20:08 +0700

    refactoring App.vue to PUG & SASS

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,33 @@
-<template>
-  <div id="app">
-    <div class="welcome">
-      <router-view></router-view>
-    </div>
-  </div>
+<template lang="pug">
+  div#app
+    router-view
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: "app",
 };
 </script>
 
-<style>
-@import url('https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap');
-@import url('https://fonts.googleapis.com/css?family=Poppins:400,500&display=swap');
-@import url('https://fonts.googleapis.com/css?family=Quicksand:400,500&display=swap');
-@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400&display=swap');
-#app {
+<style lang="sass">
+@import url("https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap")
+@import url("https://fonts.googleapis.com/css?family=Poppins:400,500&display=swap")
+@import url("https://fonts.googleapis.com/css?family=Quicksand:400,500&display=swap")
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro:400&display=swap")
+
+#app
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
 
-* {
+*
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
 
 html,
 body,
-.welcome,
-#app {
+#app
   width: 100%;
   height: 100%;
-}
 </style>
